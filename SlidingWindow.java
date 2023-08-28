40888fba7ce3ded784c77a9de3c4565406a0092d
Ajay-Babare <98898406+Ajay-Babare@users.noreply.github.com>
2023-08-28 14:46:56 +0530

    Queue Programs

---
+++ SlidingWindow.java
@@ -0,0 +1,46 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SlidingWindow {
+	public static int[] maxSlidingWindow(int[] a, int k) {
+		int n = a.length;
+		int[] r = new int[n-k+1];
+		int ri = 0;
+		
+		//store index
+		Deque<Integer> q = new ArrayDeque<>();
+		
+		for(int i=0; i<a.length;i++) {
+			
+			//remove number out of range k
+			while(!q.isEmpty() && q.peek()<i-k+1) {
+				q.poll();
+			}
+			
+			//remove smaller number in k range as they are useless
+			while(!q.isEmpty() && a[q.peekLast()] < a[i]) {
+				q.pollLast();
+			}
+			
+			//q contains index... r contains content
+			q.offer(i);
+			if(i >= k-1) {
+				r[ri++] = a[q.peek()];
+			}
+		}
+		return r;
+	}
+	public static void main(String[] args) {
+		int [] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+		int n = 8;
+		int k = 3;
+		int[] res = new int[n-k+1];
+		res = maxSlidingWindow(arr, k);
+		
+		System.out.println("the desired output is : ");
+		
+		for(int i=0; i<n-k+1; i++) {
+			System.out.println(res[i]+ " ");
+		}	
+	}
+}
---
+++ StackUsingQueue.java
@@ -0,0 +1,51 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class StackUsingQueue {
+	static Queue<Integer> q1 = new LinkedList<>();
+	static Queue<Integer> q2 = new LinkedList<>();
+	
+	//add an element in the stack
+	//time complexity : O(n)
+	static void add(int data) {
+		//1. Move all the data element from q1 to q2
+		while(!q1.isEmpty()) {
+			q2.add(q1.peek());
+			q1.poll();
+		}
+		
+		//2. Insert the new data in the q1
+		q1.add(data);
+		
+		//3.Move back all the data elements from q2 to q1
+		while(!q2.isEmpty()) {
+			q1.add(q2.peek());
+			q2.poll();
+		}
+	}
+	
+	//to remove the element from the stack
+	//time complexity : O(1)
+	static int remove() {
+		if(q1.isEmpty()) {
+			System.out.println("Stack underflow");
+			System.exit(0);
+		}
+		
+		int ele = q1.peek();
+		q1.poll();
+		return ele;
+	}
+	public static void main(String[] args) {
+		StackUsingQueue s = new StackUsingQueue();
+		//push operation in the stack
+		s.add(2);
+		s.add(5);
+		s.add(7);
+		s.add(10);
+		
+		//pop the element from the stack
+		System.out.println("Deleted element is : "+s.remove());
+		System.out.println("Deleted element is : "+s.remove());
+	}
+}
